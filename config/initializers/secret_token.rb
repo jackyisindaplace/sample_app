ec74a3bccfba9e3d80e281fbe7f7dc9635fefc28
maxence <maxence.fromentin@dooapp.com>
2012-06-21 11:28:47 +0200

    depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4d93b7a0ee3c5ebafc211d68bad4e1f202f0d782be691084d6ec69f43d517bf0ea79dbcc08a011c9e439e1f2f6f057697d3f716283f832a30d14ca7bdb358621'
